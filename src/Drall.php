bee7c34c22edff7067b87b64ae693f923fb3e19a
Jigar Mehta <hello@jigarius.com>
2022-02-22 11:29:56 -0500

    Create Symfony Console app for Drall

--- src/Drall.php
+++ src/Drall.php
@@ -2,6 +2,28 @@
 
 namespace Drall;
 
-class Drall {
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class Drall extends Application {
+
+  const NAME = 'Drall';
+
+  /**
+   * Creates a Phpake Application instance.
+   */
+  public function __construct() {
+    parent::__construct();
+
+    $this->setName(self::NAME);
+    $this->setVersion('0.0.0');
+
+    $this->input = new ArgvInput();
+    $this->output = new ConsoleOutput();
+    $this->configureIO($this->input, $this->output);
+  }
 
 }
