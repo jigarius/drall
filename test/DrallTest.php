397f49d0bc8d9573542e7181b36da08734589a2d
Jigar Mehta <hello@jigarius.com>
2022-03-01 09:28:46 -0500

    Test Drall\Drall

---
+++ test/DrallTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Drall\Drall;
+use Drall\TestCase;
+
+/**
+ * @covers \Drall\Drall
+ */
+class DrallTest extends TestCase {
+
+  public function testName() {
+    $app = new Drall();
+    $this->assertSame(Drall::NAME, $app->getName());
+  }
+
+  public function testVersion() {
+    $app = new Drall();
+    $this->assertMatchesRegularExpression(
+      '/^\d+\.\d+.\d+(-(alpha|beta|rc)\.\d+)?$/',
+      $app->getVersion()
+    );
+
+    $json_path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.json';
+    $json_data = json_decode(file_get_contents($json_path));
+
+    $this->assertNotEmpty($json_data->version);
+    $this->assertEquals($json_data->version, $app->getVersion());
+  }
+
+}
