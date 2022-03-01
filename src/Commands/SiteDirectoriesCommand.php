ed998e9d1369d883e5578242752f4b78fb474355
Jigar Mehta <hello@jigarius.com>
2022-02-28 22:47:08 -0500

    Support --drall-group with site:directories command

--- src/Commands/SiteDirectoriesCommand.php
+++ src/Commands/SiteDirectoriesCommand.php
@@ -3,6 +3,7 @@
 namespace Drall\Commands;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -14,10 +15,18 @@ class SiteDirectoriesCommand extends BaseCommand {
     $this->setName('site:directories');
     $this->setAliases(['sd']);
     $this->setDescription('Get a list of site directories.');
+    $this->addOption(
+      'drall-group',
+      NULL,
+      InputOption::VALUE_REQUIRED,
+      'Site group identifier.'
+    );
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $dirNames = $this->siteDetector()->getSiteDirNames();
+    $dirNames = $this->siteDetector()->getSiteDirNames(
+      $input->getOption('drall-group')
+    );
 
     if (count($dirNames) === 0) {
       $this->logger->warning('No site directories found.');
