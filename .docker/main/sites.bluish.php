b58609cb76e0cd73b431eeb3fe836d5605f32783
Jigar Mehta <hello@jigarius.com>
2022-02-28 21:24:11 -0500

    Create 2 site groups for development and testing

---
+++ .docker/main/sites.bluish.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Site group containing ninja turtles with a bluish bandana.
+ */
+
+$sites['donnie.drall.local'] = 'donnie';
+$sites['leo.drall.local'] = 'leo';
---
+++ .docker/main/sites.reddish.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Site group containing ninja turtles with a reddish bandana.
+ */
+
+$sites['mikey.drall.local'] = 'mikey';
+$sites['ralph.drall.local'] = 'ralph';
