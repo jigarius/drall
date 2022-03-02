de795343d80d6cfb58c8d80736e2fc9350dd6cca
Jigar Mehta <hello@jigarius.com>
2022-03-02 16:44:25 -0500

    Remove methods: configureIO() and run()

--- src/Drall.php
+++ src/Drall.php
@@ -29,15 +29,18 @@ final class Drall extends Application {
   /**
    * Creates a Phpake Application instance.
    */
-  public function __construct() {
+  public function __construct(
+    ?InputInterface $input = NULL,
+    ?OutputInterface $output = NULL
+  ) {
     parent::__construct();
+
+    $input = $input ?? new ArgvInput();
+    $output = $output ?? new ConsoleOutput();
 
     $this->setName(self::NAME);
     $this->setVersion(self::VERSION);
-
-    $this->input = new ArgvInput();
-    $this->output = new ConsoleOutput();
-    $this->configureIO($this->input, $this->output);
+    $this->configureIO($input, $output);
 
     // @todo Use dependency injection.
     $siteDetector = new SiteDetector(
@@ -45,7 +48,7 @@ final class Drall extends Application {
       $this->getSiteAliasManager()
     );
 
-    $this->setLogger(new ConsoleLogger($this->output));
+    $this->setLogger(new ConsoleLogger($output));
 
     $cmd = new SiteDirectoriesCommand();
     $cmd->setSiteDetector($siteDetector);
@@ -63,28 +66,6 @@ final class Drall extends Application {
     $this->add($cmd);
   }
 
-  /**
-   * {@inheritdoc}
-   *
-   * Based on Drush/Application::configureIO().
-   */
-  protected function configureIO(InputInterface $input, OutputInterface $output) {
-    parent::configureIO($input, $output);
-
-    // Symfony will set these later, but we want it set upfront.
-    if ($input->getParameterOption(['--verbose', '-v'], FALSE, TRUE) !== FALSE) {
-      $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
-    }
-    // We are not using "very verbose", but set this for completeness.
-    if ($input->getParameterOption(['-vv'], FALSE, TRUE) !== FALSE) {
-      $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
-    }
-    // Use -vvv of --debug for even more verbose logging.
-    if ($input->getParameterOption(['--debug', '-d', '-vvv'], FALSE, TRUE) !== FALSE) {
-      $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
-    }
-  }
-
   private function getDrupalFinder(): DrupalFinder {
     $drupalFinder = new DrupalFinder();
     $drupalFinder->locateRoot(getcwd());
@@ -96,10 +77,6 @@ final class Drall extends Application {
     $siteAliasManager = new SiteAliasManager($aliasFileLoader);
     $siteAliasManager->addSearchLocation('drush/sites');
     return $siteAliasManager;
-  }
-
-  public function run(InputInterface $input = NULL, OutputInterface $output = NULL): int {
-    return parent::run($input ?? $this->input, $output ?? $this->output);
   }
 
 }
