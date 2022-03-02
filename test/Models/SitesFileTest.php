b499ab2c8fcc134266ddff10317e9dd4e6218423
Jigar Mehta <hello@jigarius.com>
2022-03-01 21:40:08 -0500

    Test Drall\Models\SitesFile

---
+++ test/Models/SitesFileTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Drall\TestCase;
+use Drall\Models\SitesFile;
+
+/**
+ * @covers \Drall\Models\SitesFile
+ */
+class SitesFileTest extends TestCase {
+
+  protected SitesFile $subject;
+
+  public function setUp(): void {
+    $this->subject = new SitesFile($this->fixturesDir() . '/sites.valid.php');
+  }
+
+  public function testGetPath() {
+    $this->assertEquals(
+      $this->fixturesDir() . '/sites.valid.php',
+      $this->subject->getPath()
+    );
+  }
+
+  public function testInvalidPath() {
+    $this->expectException(\RuntimeException::class);
+    new SitesFile($this->fixturesDir() . '/sites.invalid.php');
+  }
+
+  public function testSitesNotDefined() {
+    $this->expectException(\RuntimeException::class);
+
+    $path = $this->createTempFile('<?php // $sites not defined.');
+    new SitesFile($path);
+  }
+
+  public function testSitesNotArray() {
+    $this->expectException(\RuntimeException::class);
+
+    $path = $this->createTempFile('<?php $sites = TRUE;');
+    new SitesFile($path);
+  }
+
+  public function testGetDirNames() {
+    $this->assertEquals(
+      ['default', 'donnie', 'leo', 'mikey', 'ralph'],
+      $this->subject->getDirNames()
+    );
+  }
+
+}
---
+++ test/fixtures/sites.valid.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * A valid sites.php file.
+ */
+
+$sites['tmnt.drall.local'] = 'default';
+$sites['donnie.drall.local'] = 'donnie';
+$sites['leo.drall.local'] = 'leo';
+$sites['mikey.drall.local'] = 'mikey';
+$sites['ralph.drall.local'] = 'ralph';
