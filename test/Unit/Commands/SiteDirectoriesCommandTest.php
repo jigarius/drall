f6625d406a1bc6130003a1588af74eddf1745047
Jigar Mehta <hello@jigarius.com>
2022-03-03 18:42:50 -0500

    Test Drall\Commands\SiteDirectoriesCommand

---
+++ test/Unit/Commands/SiteDirectoriesCommandTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use Consolidation\SiteAlias\SiteAliasManager;
+use Symfony\Component\Console\Tester\CommandTester;
+use DrupalFinder\DrupalFinder;
+use Drall\Drall;
+use Drall\Services\SiteDetector;
+use Drall\TestCase;
+
+/**
+ * @covers \Drall\Commands\SiteDirectoriesCommand
+ */
+class SiteDirectoriesCommandTest extends TestCase {
+
+  public function testExecute() {
+    $drupalFinder = new DrupalFinder();
+    $siteAliasManager = new SiteAliasManager();
+
+    $siteDetectorMock = $this->getMockBuilder(SiteDetector::class)
+      ->setConstructorArgs([$drupalFinder, $siteAliasManager])
+      ->onlyMethods(['getSiteDirNames'])
+      ->getMock();
+    $siteDetectorMock
+      ->expects($this->once())
+      ->method('getSiteDirNames')
+      ->willReturn(['donnie', 'leo']);
+
+    $app = new Drall($siteDetectorMock);
+    $tester = new CommandTester($app->find('site:directories'));
+    $tester->execute([]);
+
+    $tester->assertCommandIsSuccessful();
+
+    $this->assertEquals(
+      <<<EOF
+donnie
+leo
+
+EOF
+,
+      $tester->getDisplay()
+    );
+  }
+
+  public function testExecuteWithGroup() {
+    $drupalFinder = new DrupalFinder();
+    $siteAliasManager = new SiteAliasManager();
+
+    $siteDetectorMock = $this->getMockBuilder(SiteDetector::class)
+      ->setConstructorArgs([$drupalFinder, $siteAliasManager])
+      ->onlyMethods(['getSiteDirNames'])
+      ->getMock();
+    $siteDetectorMock
+      ->expects($this->once())
+      ->method('getSiteDirNames')
+      ->with('bluish')
+      ->willReturn(['default']);
+
+    $app = new Drall($siteDetectorMock);
+    $tester = new CommandTester($app->find('site:directories'));
+    $tester->execute(['--drall-group' => 'bluish']);
+
+    $tester->assertCommandIsSuccessful();
+  }
+
+  public function testExecuteWithNoSiteDirectories() {
+    $this->markTestSkipped('@todo Figure out a way to capture warning output.');
+  }
+
+}
