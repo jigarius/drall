93b8af13c6e02ed417bead50d15cf606ee62f01f
Jigar Mehta <hello@jigarius.com>
2022-03-04 06:52:23 -0500

    Unit test warnings when no sites/aliases are found

--- test/Unit/Commands/SiteDirectoriesCommandTest.php
+++ test/Unit/Commands/SiteDirectoriesCommandTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Consolidation\SiteAlias\SiteAliasManager;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Tester\CommandTester;
 use DrupalFinder\DrupalFinder;
 use Drall\Drall;
@@ -64,7 +65,28 @@ EOF
   }
 
   public function testExecuteWithNoSiteDirectories() {
-    $this->markTestSkipped('@todo Figure out a way to capture warning output.');
+    $drupalFinder = new DrupalFinder();
+    $siteAliasManager = new SiteAliasManager();
+
+    $siteDetectorMock = $this->getMockBuilder(SiteDetector::class)
+      ->setConstructorArgs([$drupalFinder, $siteAliasManager])
+      ->onlyMethods(['getSiteDirNames'])
+      ->getMock();
+    $siteDetectorMock
+      ->expects($this->once())
+      ->method('getSiteDirNames')
+      ->willReturn([]);
+
+    $output = new BufferedOutput();
+
+    $app = new Drall($siteDetectorMock, NULL, $output);
+    $tester = new CommandTester($app->find('site:directories'));
+    $tester->execute([]);
+
+    $tester->assertCommandIsSuccessful();
+
+    $this->assertEquals('', $tester->getDisplay());
+    $this->assertEquals("[warning] No Drupal sites found.\n", $output->fetch());
   }
 
 }
