fb31b1f043992e33e735f1dea3855d6faf8b6601
Jigar Mehta <hello@jigarius.com>
2022-03-04 08:15:48 -0500

    Add class comment

--- src/Runners/PassthruRunner.php
+++ src/Runners/PassthruRunner.php
@@ -2,6 +2,9 @@
 
 namespace Drall\Runners;
 
+/**
+ * A runner that uses PHP's passthru to execute commands.
+ */
 class PassthruRunner implements RunnerInterface {
 
   public function execute(string $command): int {
