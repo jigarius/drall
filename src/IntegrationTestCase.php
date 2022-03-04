2074b8fddd0023ba5112e8f8d01294f420e8af59
Jigar Mehta <hello@jigarius.com>
2022-03-04 00:07:18 -0500

    Create class: IntegrationTestCase

---
+++ src/IntegrationTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drall;
+
+class IntegrationTestCase extends TestCase {
+
+  /**
+   * Original current working directory.
+   *
+   * @var string
+   */
+  protected string $cwd;
+
+  protected function setUp(): void {
+    $this->cwd = getcwd();
+    chdir($this->drupalDir());
+  }
+
+  protected function tearDown(): void {
+    chdir($this->cwd);
+  }
+
+  /**
+   * Asserts whether shell output equality.
+   *
+   * Ignores empty spaces at the end of lines.
+   *
+   * @param string $expected
+   *   Expected output.
+   * @param mixed $actual
+   *   Actual output.
+   * @param string $message
+   *   Error message.
+   */
+  protected function assertOutputEquals(string $expected, $actual, string $message = ''): void {
+    $actual = preg_replace('@(\s+)\n@', "\n", $actual);
+    $this->assertEquals($expected, $actual, $message);
+  }
+
+}
