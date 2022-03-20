da0dd99a4cb0c76bdefda01234c93a9d5550972c
Jigar Mehta <hello@jigarius.com>
2022-03-19 20:43:37 -0400

    Bump version: v1.1.0-rc1

--- src/Drall.php
+++ src/Drall.php
@@ -22,7 +22,7 @@ final class Drall extends Application {
 
   const NAME = 'Drall';
 
-  const VERSION = '1.0.0-beta1';
+  const VERSION = '1.1.0-rc1';
 
   use LoggerAwareTrait;
   use SiteDetectorAwareTrait;
