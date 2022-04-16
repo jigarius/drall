eb056505d9859842c0aa914e49721b964f80637a
Jigar Mehta <hello@jigarius.com>
2022-04-15 23:23:42 -0400

    Create ExecRunner which captures output

--- src/Runners/PassthruRunner.php
+++ src/Runners/PassthruRunner.php
@@ -3,13 +3,17 @@
 namespace Drall\Runners;
 
 /**
- * A runner that uses PHP's passthru to execute commands.
+ * A runner that uses PHP's passthru() to execute commands.
  */
 class PassthruRunner implements RunnerInterface {
 
   public function execute(string $command): int {
     passthru($command, $exitCode);
     return $exitCode;
+  }
+
+  public function getOutput(): ?string {
+    return NULL;
   }
 
 }
