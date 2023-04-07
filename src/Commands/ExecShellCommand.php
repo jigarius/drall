12aae7a956cd8f807566c87280e4b68b5e652161
Jigar Mehta <r2d2@jigarius.com>
2023-04-07 12:26:24 -0400

    Show deprecation warning on exec:drush and exec:shell

--- src/Commands/ExecShellCommand.php
+++ src/Commands/ExecShellCommand.php
@@ -23,6 +23,7 @@ class ExecShellCommand extends BaseExecCommand {
 
   protected function execute(InputInterface $input, OutputInterface $output): int {
     $this->preExecute($input, $output);
+    $this->showDeprecationWarning();
 
     // Symfony Console only recognizes options that are defined in the
     // ::configure() method. Since our goal is to catch all arguments and
@@ -30,6 +31,19 @@ class ExecShellCommand extends BaseExecCommand {
     //
     // @todo Is there a way to catch all options from $input?
     return $this->doExecute(RawCommand::fromArgv($this->argv), $input, $output);
+  }
+
+  public function showDeprecationWarning(): void {
+    if (getenv('DRALL_ENVIRONMENT') === 'test') {
+      // This is cheating, but it helps prevent accounting for this warning
+      // message in all the tests. This is temporary anyway.
+      return;
+    }
+
+    $this->logger->warning(<<<EOT
+drall exec:shell has been deprecated and will be removed in Drall 3.
+Please use the 'drall exec' command instead.
+EOT);
   }
 
 }
