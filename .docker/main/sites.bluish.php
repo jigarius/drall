ac1d6386de4fab94fe6945f11c4aa1787f8f9d88
Jigar Mehta <r2d2@jigarius.com>
2023-04-08 13:10:20 -0400

    Create sites with multiple keys

--- .docker/main/sites.bluish.php
+++ .docker/main/sites.bluish.php
@@ -5,5 +5,9 @@
  * Site group containing ninja turtles with a bluish bandana.
  */
 
+$sites['donatello.com'] = 'donnie';
+$sites['8080.donatello.com'] = 'donnie';
 $sites['donnie.drall.local'] = 'donnie';
+
+$sites['leonardo.com'] = 'leo';
 $sites['leo.drall.local'] = 'leo';
--- test/fixtures/sites.valid.php
+++ test/fixtures/sites.valid.php
@@ -5,8 +5,19 @@
  * A valid sites.php file.
  */
 
+$sites['tmnt.com'] = 'default';
+$sites['cowabunga.com'] = 'default';
 $sites['tmnt.drall.local'] = 'default';
+
+$sites['donatello.com'] = 'donnie';
+$sites['8080.donatello.com'] = 'donnie';
 $sites['donnie.drall.local'] = 'donnie';
+
+$sites['leonardo.com'] = 'leo';
 $sites['leo.drall.local'] = 'leo';
+
+$sites['michelangelo.com'] = 'mikey';
 $sites['mikey.drall.local'] = 'mikey';
+
+$sites['raphael.com'] = 'ralph';
 $sites['ralph.drall.local'] = 'ralph';
