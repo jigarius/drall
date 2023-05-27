f203a8ebdf2fe3098f5046ff6991e0165be38570
Jigar Mehta <r2d2@jigarius.com>
2023-05-27 12:43:26 -0300

    Create trait: SignalAwareTrait

---
+++ src/Trait/SignalAwareTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drall\Trait;
+
+trait SignalAwareTrait {
+
+  /**
+   * Register a signal listener.
+   *
+   * @param int $signo
+   *   Signal Number.
+   * @param callable $handler
+   *   A callable event listener.
+   *
+   * @return bool
+   *   True if the listener was registered.
+   */
+  protected function registerSignalListener(int $signo, callable $handler): bool {
+    if (!extension_loaded('pcntl')) {
+      return FALSE;
+    }
+
+    declare(ticks = 1);
+    \pcntl_signal($signo, $handler);
+
+    return TRUE;
+  }
+
+}
